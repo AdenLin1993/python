e40867f8c2a777121ac688c7c309c2db78bad365
AdenLin1993 <55698832+AdenLin1993@users.noreply.github.com>
2020-09-06 23:08:28 +0800

    Update fetchmariadb.py

--- fetchmariadb.py
+++ fetchmariadb.py
@@ -20,8 +20,10 @@ import logging
 import textwrap
 import pandas as pd
 
+""" 创建一个mariadb数据库拉取数据的类"""
 """第一種方法"""
 class Fetch_pymysql(object) :
+	"""重写类的初始化属性"""
 	def __init__(self) :
 		self.config = configparser.RawConfigParser()
 		self.config.read('./config.cfg')
@@ -33,6 +35,8 @@ class Fetch_pymysql(object) :
 			, level=logging.INFO
 			, format='%(asctime)s %(message)s'
 			, datefmt='%Y/%m/%d %I:%M:%S %p')
+		
+		"""建立与mariadb数据库连接的客户端"""
 		try :
 			self.MySqlConn = pymysql.connect(host=self.config.get('ZS_IT_MySQLR','mysqlserver')
 				,user=self.config.get('ZS_IT_MySQLR','mysqluser')
@@ -44,7 +48,8 @@ class Fetch_pymysql(object) :
 			print(inst)
 			logging.error('MySql Connection Fail')
 			logging.error(inst)
-
+	
+	"""这是一个从mariadb数据库拉取数据的函数范例"""
 	def fetch_xxxx(self):
 		try:
 			with self.MySqlConn.cursor() as cursor:
@@ -63,8 +68,10 @@ class Fetch_pymysql(object) :
 				logging.error(inst)
 		return meterbase
 
+""" 创建一个mariadb数据库拉取数据的类"""
 """第二種方法"""
 class Fetch_sqlalchemy(object) :
+	"""重写类的初始化属性"""
 	def __init__(self) :
 		self.config = configparser.RawConfigParser()
 		self.config.read('./config.cfg')
@@ -81,6 +88,8 @@ class Fetch_sqlalchemy(object) :
 		self.user = self.config.get('ZS_IT_MySQLW','mysqluser')
 		self.pwd = self.config.get('ZS_IT_MySQLW','mysqlpassword')
 		self.db = self.config.get('ZS_IT_MySQLW','database')
+		
+		"""建立与mariadb数据库连接的客户端（可以这样理解，专业术语是orm）"""
 		try:
 			sqlEngine = create_engine("mysql+pymysql://{0}:{1}@{2}:3306/{3}?charset=utf8".format(self.user,self.pwd,self.host,self.db))
 			self.dbConnection = sqlEngine.connect()
@@ -89,7 +98,8 @@ class Fetch_sqlalchemy(object) :
 			print(inst)
 			logging.error('MySql Connection Fail')
 			logging.error(inst)
-
+			
+	"""这是一个从mariadb数据库拉取数据的函数范例"""
 	def fetch_xxxx(self):
 		try:
 			sql = "SELECT * FROM dat_energy.wzs_sb_base_data WHERE wzs_sb_base_data.batchid = (SELECT MAX(wzs_sb_base_data.batchid) FROM dat_energy.wzs_sb_base_data); "
