f23f8792c45a5b519b7be676a3c1027344a3d414
AdenLin1993 <55698832+AdenLin1993@users.noreply.github.com>
2020-09-06 20:22:29 +0800

    Update fetches.py

--- fetches.py
+++ fetches.py
@@ -12,7 +12,9 @@ import re
 import copy										
 from elasticsearch import Elasticsearch				
 
+""" 创建一个从ES数据库拉取数据的类"""
 class Fetch(object) :
+	"""重写类的初始化属性"""
 	def __init__(self) :
 		self.Index = ["fem_meterreading_*"]
 		
@@ -22,12 +24,15 @@ class Fetch(object) :
 			, level=logging.INFO
 			, format='%(asctime)s %(message)s'
 			, datefmt='%Y/%m/%d %I:%M:%S %p')
+		
+		"""建立与ES数据库连接的客户端"""
 		try :
 			self.EsClient = Elasticsearch(["zsarm-emnrdb-p01.wzs.wistron","zsarm-emnrdb-p02.wzs.wistron","zsarm-emnrdb-p03.wzs.wistron"],maxsize = 25,timeout = 180)
 		except Exception as inst :
 			print('Elasticsearch connect fail')
 			print(inst)
-
+			
+	"""这是一个从ES数据库拉取数据的函数范例"""
 	def meter_hour_cur(self,cur_flagtime):
 		print('fetch {0} Start'.format(self.Index))
 		logging.info('fetch {0} Start'.format(self.Index))
@@ -37,6 +42,7 @@ class Fetch(object) :
 		Cur_EndDate_timestamp = cur_flagtime*1000
 
 		""" get cur hour reading  """
+		""" query_body 相当于SQL中的查询限制语句 where"""
 		query_body = {
 			"query": {
 				"bool" : {
@@ -68,8 +74,14 @@ class Fetch(object) :
 
 			query_body['query']['bool']['must'][0]['range']['evt_dt']['from'] = hour_Cur_StartDate_timestamp
 			query_body['query']['bool']['must'][0]['range']['evt_dt']['to'] = hour_Cur_EndDate_timestamp
+			
+			"""通过创建好的ES客户端和query_body从ES数据库拉取相应的数据，如下，page就是我们拉取到的文件内容"""
 			page = self.EsClient.search(index=self.Index, size=100000, body=query_body, scroll='50m')
-
+			
+			"""如下，total就是page文件内容总记录条数"""
+			total = page["hits"]["total"]
+			
+			"""如下，page["hits"]["hits"]就是page文件内容的可迭代对象，存储每一条记录内容,通过遍历这个迭代对象处理这些数据。"""
 			for oneHit in page["hits"]["hits"] :
 				"""如果篩選的內容中有異常數據，跳過處理"""
 				try :
