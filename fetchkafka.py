ff436f5946164953be3a6759ab6e797ffb98cef8
AdenLin1993 <55698832+AdenLin1993@users.noreply.github.com>
2020-09-06 22:42:12 +0800

    Update fetchkafka.py

--- fetchkafka.py
+++ fetchkafka.py
@@ -10,8 +10,11 @@ from bson.json_util import dumps,loads,JSONOptions,DEFAULT_JSON_OPTIONS
 import pandas as pd
 import numpy as np
 
+
+""" 创建一个从KAFKA数据库拉取数据的类"""
 """從未加認證的主機接數據"""
 class Fetch(object) :
+	"""重写类的初始化属性"""
 	def __init__(self) :
 		self.config = configparser.RawConfigParser()
 		self.config.read('./config.cfg')
@@ -21,6 +24,8 @@ class Fetch(object) :
 			, level=logging.INFO
 			, format='%(asctime)s %(message)s'
 			, datefmt='%Y/%m/%d %I:%M:%S %p')
+		
+		"""建立与kafka数据库连接的客户端，group.id随意命名，保证其他人没有在使用这个id，因为一个id只能拉取一次数据，每次拉取数据都拉取七天前到现在的数据"""
 		try :
 			Source_Kafka_Consumer = Consumer({
 					'bootstrap.servers':'10.41.241.6:9092'
@@ -36,18 +41,21 @@ class Fetch(object) :
 			print(inst)
 			logging.error('kafaka Connection Fail')
 			logging.error(inst)
-
+	
+	"""这是一个从kafka数据库拉取数据的函数范例"""
 	def fetch_xxxx(self):
 		try:
 			onelist = []
 			count = 0
 			while True:
+				"""通过创建好连接kafka客服端和相关参数，拉取数据"""
 				msg = self.consumer.poll(1)
 				if msg is None:
 					continue
 				if msg.error():
 					print('Consumer error: {}'.format(msg.error()))
 					continue
+				"""取出数据，和存储数据"""
 				data = json_util.loads(msg.value())
 				onelist.append(data)
 				count = count + 1
